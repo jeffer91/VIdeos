9b1df96a74e01bc0a503b067a05812701ce890d3
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-09-08 23:06:46 -0500

    Expose local library APIs to renderer

---
+++ electron/preload.cjs
@@ -0,0 +1,12 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('videosStudio', {
+  library: {
+    importMedia: (options) => ipcRenderer.invoke('library:import', options),
+    listMedia: (options) => ipcRenderer.invoke('library:list', options),
+    deleteMedia: (options) => ipcRenderer.invoke('library:delete', options),
+    setDefault: (options) => ipcRenderer.invoke('library:set-default', options),
+    getDefaults: () => ipcRenderer.invoke('library:get-defaults'),
+    reveal: (options) => ipcRenderer.invoke('library:reveal', options),
+  },
+});
