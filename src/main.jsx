55396b0d55bf6ae329d5775535923280647e699d
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-09-08 23:13:27 -0500

    Load production workflow styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import ReactDOM from 'react-dom/client';
-import StudioApp from './StudioApp';
+import App from './App';
 import './styles.css';
 import './compact.css';
 import './studio.css';
 import './format-rules.css';
+import './production.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<StudioApp />);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
