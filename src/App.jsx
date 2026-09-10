810acc9079595311901078285214ce21d666aa4c
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-09-09 21:02:37 -0500

    Integrate guided workflow and scene visuals

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import ProductionApp from './ProductionApp';
 import TemplateManager from './TemplateManager';
+import VisualManager from './VisualManager';
+import WorkflowEnhancer from './WorkflowEnhancer';
 
 export default function App() {
   return (
     <>
       <ProductionApp />
       <TemplateManager />
+      <VisualManager />
+      <WorkflowEnhancer />
     </>
   );
 }
