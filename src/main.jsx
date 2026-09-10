8c852b65ba412ba653dbcfdd18dfdec2ba9ecec4
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-09-09 21:02:44 -0500

    Load final UX polish styles

--- src/main.jsx
+++ src/main.jsx
@@ -7,5 +7,6 @@ import './format-rules.css';
 import './production.css';
 import './audit.css';
 import './light-theme.css';
+import './ux-enhancements.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
