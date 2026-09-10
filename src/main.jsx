ae04c3aa3fd394da073affb454a99eaacd77ed55
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-09-09 19:18:58 -0500

    Load light Videos Studio theme

--- src/main.jsx
+++ src/main.jsx
@@ -6,5 +6,6 @@ import './studio.css';
 import './format-rules.css';
 import './production.css';
 import './audit.css';
+import './light-theme.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
