83c09c15f99fee47263c5a5e281e4a3ace7e5952
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-09-12 22:52:29 -0500

    Add 11 Records channel profile

---
+++ src/channel.js
@@ -0,0 +1,6 @@
+export const CHANNEL_PROFILE = Object.freeze({
+  name: '11 Records',
+  niche: 'Récords y marcas históricas del fútbol mundial',
+  language: 'es',
+  finalCta: 'Suscríbete a 11 Records para más récords brutales del fútbol mundial.',
+});
